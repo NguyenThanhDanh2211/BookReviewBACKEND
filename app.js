6926b7bb1acfd6f76d1ef3cce6e297a8d1010609
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2023-04-18 15:04:24 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -16,4 +16,16 @@ app.use("/api/auth", authRouter);
 const bookRouter = require("./app/routes/book.route");
 app.use("/api/book", bookRouter);
 
+// Cai dat su ly loi
+const ApiError = require("./app/api-error");
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
